bb608acff304d3495c3e17c3afc8df7da9e93f4c
LEI WANG <william.wang@meitrust.com>
2024-10-09 17:23:23 +0800

    change env config

--- lib/api/move.ts
+++ lib/api/move.ts
@@ -1,17 +1,14 @@
 import { throwExpression } from "../shared/utils";
 
-export const EXAMPLE_MOVE_PACKAGE_ID =
-  process.env.EXAMPLE_MOVE_PACKAGE_ID ??
-  throwExpression(new Error("EXAMPLE_MOVE_PACKAGE_ID not configured"));
+//console.log('process.env.EXAMPLE_MOVE_PACKAGE_ID', JSON.stringify(process.env.EXAMPLE_MOVE_PACKAGE_ID))
+export const EXAMPLE_MOVE_PACKAGE_ID = process.env.EXAMPLE_MOVE_PACKAGE_ID || '';
+if (!process.env.EXAMPLE_MOVE_PACKAGE_ID) {
+  console.warn("Warning: EXAMPLE_MOVE_PACKAGE_ID is not set");
+}
 
-export const API_HOST =
-  process.env.API_HOST ??
-  throwExpression(new Error("API_HOST not configured"));
+export const API_HOST = process.env.API_HOST || '';
+if (!process.env.API_HOST) {
+  console.warn("Warning: API_HOST is not set");
+}
 
-// export const PASSKEYS_API_KEY =
-//   process.env.PASSKEYS_API_KEY ??
-//   throwExpression(new Error("PASSKEYS_API_KEY not configured"));
-
-// export const PASSKEYS_TENANT_ID =
-//   process.env.NEXT_PUBLIC_PASSKEYS_TENANT_ID ??
-//   throwExpression(new Error("NEXT_PUBLIC_PASSKEYS_TENANT_ID not configured"));
+//export const API_HOST = ''
